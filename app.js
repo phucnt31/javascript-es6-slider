4dabc17613057075be2b5523db8efe80380522ef
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-29 15:47:05 +0700

    Next slide function

--- app.js
+++ app.js
@@ -26,3 +26,20 @@ container.innerHTML = people
         </article>`;
   })
   .join("");
+
+const startSlider = (type) => {
+  const active = document.querySelector(".active");
+  const last = document.querySelector(".last");
+  let next = active.nextElementSibling;
+
+  active.classList.remove("active");
+  last.classList.remove("last");
+  next.classList.remove("next");
+
+  active.classList.add("last");
+  last.classList.add("next");
+  next.classList.add("active");
+};
+
+nextBtn.addEventListener("click", () => startSlider());
+prevBtn.addEventListener("click", () => startSlider("prev"));
